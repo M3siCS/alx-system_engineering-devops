abd49c1cde6f76428b5e6fc296dd6d38fc92d86d
M3siCS <hadeelkaddy86@gmail.com>
2024-07-04 17:47:30 -0700

    go

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
-# Installs puppet-lint
+# 1-install_a_package.pp
 
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
+package { 'python3-pip':
+  ensure => installed,
 }
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  creates => '/usr/local/lib/python3.8/dist-packages/flask',
+}
+
+file { '/usr/bin/flask':
+  ensure => link,
+  target => '/usr/local/bin/flask',
+}
+
