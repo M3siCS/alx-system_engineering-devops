8a20dabb46431eccdbc7d62586d792069ebc7100
M3siCS <hadeelkaddy86@gmail.com>
2024-07-14 10:06:20 -0700

    fixed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-# Installs puppet-lint
+# 1-install_a_package.pp
 
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
 }
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['Flask'],
+}
+
