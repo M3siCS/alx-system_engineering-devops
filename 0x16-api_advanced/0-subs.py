e420fec4035d8cfac3c738570c7ea2601e560403
M3siCS <hadeelkaddy86@gmail.com>
2024-08-19 14:56:46 -0700

    run

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,10 +17,13 @@ def number_of_subscribers(subreddit):
 
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
+    response = get(url, headers=user_agent, allow_redirects=False)
 
-    try:
-        results = response.json()
-        return results.get('data', {}).get('subscribers', 0)
-    except Exception:
+    if response.status_code == 200:
+        try:
+            results = response.json()
+            return results.get('data', {}).get('subscribers', 0)
+        except Exception:
+            return 0
+    else:
         return 0
